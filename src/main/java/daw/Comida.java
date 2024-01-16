bc3fd92100d1ca7727adef8ee0b9d23a50e53a47
clara <calvara976@g.educaand.es>
2024-01-16 12:26:38 +0100

    creada la clase comida

--- src/main/java/daw/Comida.java
+++ src/main/java/daw/Comida.java
@@ -9,5 +9,34 @@ package daw;
  * @author clara
  */
 public class Comida extends Producto {
+    //atributos
+    private String descripcionComida;
+    
+    //constructor
+    public Comida(String descripcionComida, String codProducto, String nombre, 
+            double precioSinIVA, double precioConIVA) {
+        super(codProducto, nombre, precioSinIVA, precioConIVA);
+        this.descripcionComida = descripcionComida;
+    }
+    
+    //getter y setter
+    public String getDescripcionComida() {
+        return descripcionComida;
+    }
+
+    public void setDescripcionComida(String descripcionComida) {
+        this.descripcionComida = descripcionComida;
+    }
+    
+    //toString
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Comida{");
+        sb.append("Descripción: ").append(descripcionComida);
+        sb.append('}');
+        return sb.toString();
+    }
     
 }
