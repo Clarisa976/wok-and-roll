6e4e649f667b31e50645588e55f537555188abe0
clara <calvara976@g.educaand.es>
2024-01-16 11:55:23 +0100

    creación del proyecto DawFood

---
+++ src/main/java/daw/DawFoodClaraMiguel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author clara
+ */
+public class DawFoodClaraMiguel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
