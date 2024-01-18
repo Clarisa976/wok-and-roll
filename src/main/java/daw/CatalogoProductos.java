4aae854caf84661a9fb8d1611f15b250e36cac1b
clara <calvara976@g.educaand.es>
2024-01-18 12:01:03 +0100

    creada la clase catalogo de productos que contiene una lista de productos y un método para calcular el precio

---
+++ src/main/java/daw/CatalogoProductos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author clara
+ */
+public class CatalogoProductos {
+    
+    //atributos
+    private final List<Producto> listaProductos;
+    
+    //constructor
+    public CatalogoProductos(List<Producto> listaProductos) {
+        this.listaProductos = new ArrayList<>();
+    }
+    //método para calcular el precio con iva
+}
