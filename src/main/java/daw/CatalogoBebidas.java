9ddff61fd194b0cfde8c51102ac40cda24b88cea
clara <calvara976@g.educaand.es>
2024-01-19 10:05:01 +0100

    modificado el catálogo para hacerlo final

--- src/main/java/daw/CatalogoBebidas.java
+++ src/main/java/daw/CatalogoBebidas.java
@@ -11,7 +11,7 @@ import java.util.List;
  *
  * @author clara
  */
-public class CatalogoBebidas {
+public final class CatalogoBebidas {
 
     //lista de bebidas
     private final List<Producto> listaBebidas;
