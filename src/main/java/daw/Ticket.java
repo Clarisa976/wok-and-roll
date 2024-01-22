33f2023dbd4cf51ded1f5e224b25c1db68d8d543
clara <calvara976@g.educaand.es>
2024-01-22 08:39:10 +0100

    D:

--- src/main/java/daw/Ticket.java
+++ src/main/java/daw/Ticket.java
@@ -16,8 +16,8 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class Ticket {
 
     //atributos    
-    private static final int contadorTickets = 1;
-    private final String numeroPedido;//random de 4 dígitos
+    private static int contadorTickets = 0;
+    private final int numeroPedido;//random de 4 dígitos
     private final List<Producto> productos;
     private final List<Integer> cantidad; //por cada producto elegido
     private final double importeTotal; //el precio de cada producto (con iva) * cantidad
@@ -29,7 +29,7 @@ public class Ticket {
         if (productos.size() != cantidad.size()) {
             throw new IllegalArgumentException("Algo no coincide.");
         }
-        this.numeroPedido = RandomStringUtils.randomNumeric(4);
+        this.numeroPedido = contadorTickets++;
         this.productos = productos;
         this.cantidad = cantidad;
         this.importeTotal = calcularImporteTotal();
@@ -41,7 +41,7 @@ public class Ticket {
         return contadorTickets;
     }
 
-    public String getNumeroPedido() {
+    public int getNumeroPedido() {
         return numeroPedido;
     }
 
